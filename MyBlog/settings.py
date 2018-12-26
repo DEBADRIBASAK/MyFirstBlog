bf029f8b38b817cfba6a11aab9476abbe3357ac2
DEBADRIBASAK <32904247+DEBADRIBASAK@users.noreply.github.com>
2018-12-26 23:07:13 +0530

    Update settings.py

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -121,5 +121,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'MyBlog/media')
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = ('./static',)
